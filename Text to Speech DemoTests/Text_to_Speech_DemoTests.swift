db22fecda952c9edcc971bc3c1de50b3872728e2
netratechnosys <dharmeshghoghari@live.com>
2018-10-10 16:30:19 +0530

    Initial Commit

---
+++ Text to Speech DemoTests/Text_to_Speech_DemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  Text_to_Speech_DemoTests.swift
+//  Text to Speech DemoTests
+//
+//  Created by Netra Technosys on 10/10/18.
+//  Copyright © 2018 Netra Technosys. All rights reserved.
+//
+
+import XCTest
+@testable import Text_to_Speech_Demo
+
+class Text_to_Speech_DemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
